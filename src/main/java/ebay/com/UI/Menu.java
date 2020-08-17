d524ec3220a520ad8fa64fb0e42f546e60a7e808
ngovanthuan <ngovanthuan989@gmail.com>
2020-08-17 15:58:35 +0700

    Ebay

---
+++ src/main/java/ebay/com/UI/Menu.java
@@ -0,0 +1,415 @@
+package ebay.com.UI;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import ebay.com.bl.Address_DetailsBL;
+import ebay.com.bl.AdminBL;
+import ebay.com.bl.Brand_ProductBL;
+import ebay.com.bl.Category_ProductBL;
+import ebay.com.bl.CustomerBL;
+import ebay.com.bl.ProductBL;
+import ebay.com.bl.SuppilerBL;
+
+public class Menu {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void ShowMenu() throws SQLException{
+        String choice;
+
+        while (true) {
+            System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~Welcome to our EBAY~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("═════════════════════════════════════════════════════════════════════════════");
+            System.out.println("|1.Home Page                                                                |");
+            System.out.println("|---------------------------------------------------------------------------|");
+            System.out.println("|2.Product Details                                                          |");
+            System.out.println("|---------------------------------------------------------------------------|");
+            System.out.println("|3.Search                                                                   |");
+            System.out.println("|---------------------------------------------------------------------------|");
+            System.out.println("|4.Customer                                                                 |");
+            System.out.println("|---------------------------------------------------------------------------|");
+            System.out.println("|5.Suppiler channel                                                         |");
+            System.out.println("|---------------------------------------------------------------------------|");
+            System.out.println("|6.Dasboard(Admin)                                                          |");
+            System.out.println("|---------------------------------------------------------------------------|");
+            System.out.println("|0.Exit The Program                                                         |");
+            System.out.println("|---------------------------------------------------------------------------|");
+            System.out.println("═════════════════════════════════════════════════════════════════════════════");
+            System.out.println("   ==>>Inviting You To Choose:            ");
+
+            choice = sc.nextLine();
+            switch (choice) {
+                case "1":
+
+                    break;
+                case "2":
+
+
+                    break;
+                case "3":
+
+
+                    break;
+                case "4":
+                    ClearTheScreen.clrscr();
+                    CustomerBL.LoginCustomer();
+                    Customer();
+
+                    break;
+                case "5":
+                    ClearTheScreen.clrscr();
+                    SuppilerBL.LoginSuppiler();
+                    Seller();
+                    break;
+                case "6":
+                    AdminBL.AdminLogin();
+                    ClearTheScreen.clrscr();
+                    Admin();
+
+                    break;
+                case "0":
+                    ClearTheScreen.clrscr();
+                    System.out.println("Bye. See You Again!");
+                    System.exit(0);
+
+                    break;
+
+                default:
+                    ClearTheScreen.clrscr();
+                    System.out.println("There is no function for this .Re-Enter:");
+                    break;
+            }
+        }
+    }
+    public static void Admin() throws SQLException{
+
+        String choice;
+        while (true) {
+            System.out.println("=======================Admin Dashboard=====================");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("|1.Seller Management                                      |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("|2.Customer Management                                    |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("|3.Product Management                                     |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("|4.ShippingMethod Management                              |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("|0.Return To The Previous Page                            |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("   ==>>Inviting You To Choose:            ");
+
+            choice = sc.nextLine();
+
+            switch (choice) {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+
+                    break;
+                case "0":
+                    ClearTheScreen.clrscr();
+                    ShowMenu();
+                    break;
+
+                default:
+                    ClearTheScreen.clrscr();
+                    System.out.println("There is no function for this .Re-Enter:");
+                    break;
+            }
+
+        }
+
+
+    }
+
+    public static void Seller() throws SQLException{
+        String choice;
+        while (true) {
+            System.out.println("=========================Seller========================");
+            System.out.println("-------------------------------------------------------");
+            System.out.println("|1.Product Management                                 |");
+            System.out.println("-------------------------------------------------------");
+            System.out.println("|2.Category Management                                |");
+            System.out.println("-------------------------------------------------------");
+            System.out.println("|3.Brand Management                                   |");
+            System.out.println("-------------------------------------------------------");
+            System.out.println("|4.Order Management                                   |");
+            System.out.println("-------------------------------------------------------");
+            System.out.println("|5.Revenue Management                                 |");
+            System.out.println("-------------------------------------------------------");
+            System.out.println("|0.Return To The Previous Page                        |");
+            System.out.println("-------------------------------------------------------");
+            System.out.println("   ==>>Inviting You To Choose:            ");
+            choice = sc .nextLine();
+
+            switch (choice) {
+                case "1":
+                    ClearTheScreen.clrscr();
+                    Product_Management();
+                    break;
+                case "2":
+                    ClearTheScreen.clrscr();
+                    Category_Management();
+                    break;
+                case "3":
+                    ClearTheScreen.clrscr();
+                    Brand_Management();
+                    break;
+                case "4":
+
+                    break;
+                case "5":
+
+                    break;
+
+                case "0":
+                    ClearTheScreen.clrscr();
+                    ShowMenu();
+                    break;
+
+                default:
+                    ClearTheScreen.clrscr();
+                    System.out.println("There is no function for this .Re-Enter:");
+                    break;
+            }
+        }
+
+    }
+
+    public static void Customer() throws SQLException{
+        String choice;
+        while (true) {
+            System.out.println("==========================Customer=========================");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("|1.Xem thông tin tài khoản                                |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("|2.Lịch sử mua hàng                                       |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("|3.Address_Shipping Management                            |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("|4.Giỏ Hàng                                               |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("|0.Return To The Previous Page                            |");
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("   ==>>Inviting You To Choose:            ");
+            choice =sc.nextLine();
+
+            switch (choice) {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+                ClearTheScreen.clrscr();
+                Address_Shipping();
+
+                    break;
+                case "4":
+
+                    break;
+
+                case "0":
+
+                    ClearTheScreen.clrscr();
+                    ShowMenu();
+
+                    break;
+
+                default:
+                    ClearTheScreen.clrscr();
+                    System.out.println("There is no function for this .Re-Enter:");
+                    break;
+            }
+        }
+
+    }
+    public static void Address_Shipping()throws SQLException{
+        String choice;
+        while (true) {
+
+            System.out.println("====================Address_Shipping_Management===================");
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("|1.Add New Address_Details                                       |");
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("|2.Update Address_Details                                        |");
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("|3.List Address_Details                                          |");
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("|0.Return To The Previous Page                                   |");
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("   ==>>Inviting You To Choose:            ");
+            choice =sc.nextLine();
+
+            switch (choice) {
+                case "1":
+                ClearTheScreen.clrscr();
+                Address_DetailsBL.add_address_details();
+                    break;
+
+                case "2":
+                ClearTheScreen.clrscr();
+                Address_DetailsBL.update_address_details();
+
+                    break;
+
+                case "3":
+                ClearTheScreen.clrscr();
+                Address_DetailsBL.show_address_details();
+
+                    break;
+
+                case "0":
+                ClearTheScreen.clrscr();
+                Customer();
+
+                    break;
+
+                default:
+                ClearTheScreen.clrscr();
+                System.out.println("There is no function for this .Re-Enter:");
+                    break;
+            }
+        }
+    }
+    public static void Category_Management() throws SQLException{
+        String choice;
+        while (true) {
+            System.out.println("====================Category_Management===================");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|1.Add New Category_Product                              |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|2.Update Category_Product                               |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|3.List Category_Product                                 |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|0.Return To The Previous Page                           |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("   ==>>Inviting You To Choose:            ");
+            choice =sc.nextLine();
+
+            switch (choice) {
+                case "1":
+                ClearTheScreen.clrscr();
+                Category_ProductBL.add_category_product();
+                    break;
+                case "2":
+                  ClearTheScreen.clrscr();
+                  Category_ProductBL.Edit_Category_Product();
+                    break;
+                case "3":
+                   ClearTheScreen.clrscr();
+                   Category_ProductBL.Show_Category_Product();
+                    break;
+                case "0":
+                    ClearTheScreen.clrscr();
+                    Seller();
+
+                    break;
+
+
+                default:
+                ClearTheScreen.clrscr();
+                System.out.println("There is no function for this .Re-Enter:");
+                    break;
+            }
+        }
+
+
+    }
+    public static void Brand_Management() throws SQLException{
+        String choice;
+        while (true) {
+            System.out.println(" ====================Brand_Management===================");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|1.Add New Brand_Product                                 |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|2.Update Brand_Product                                  |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|3.List Brand_Product                                    |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|0.Return To The Previous Page                           |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("   ==>>Inviting You To Choose:            ");
+            choice =sc.nextLine();
+
+            switch (choice) {
+                case "1":
+                    ClearTheScreen.clrscr();
+                    Brand_ProductBL.add_brand_product();
+                    break;
+                case "2":
+                  ClearTheScreen.clrscr();
+                  Brand_ProductBL.edit_brand_product();
+                    break;
+                case "3":
+                   ClearTheScreen.clrscr();
+                   Brand_ProductBL.show_brand_product();
+                    break;
+                case "0":
+                    ClearTheScreen.clrscr();
+                    Seller();
+                    break;
+
+
+                default:
+                ClearTheScreen.clrscr();
+                System.out.println("There is no function for this .Re-Enter:");
+                    break;
+            }
+        }
+
+
+    }
+    public static void Product_Management() throws SQLException{
+        String choice;
+        while (true) {
+            System.out.println(" ====================Product_Management===================");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|1.Add New Product                                       |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|2.Update Product                                        |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|3.List Product                                          |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("|0.Return To The Previous Page                           |");
+            System.out.println("----------------------------------------------------------");
+            System.out.println("   ==>>Inviting You To Choose:            ");
+            choice =sc.nextLine();
+
+            switch (choice) {
+                case "1":
+                    ClearTheScreen.clrscr();
+                    ProductBL.add_product();
+                    break;
+                case "2":
+                    ClearTheScreen.clrscr();
+                    ProductBL.update_product();
+                    break;
+                case "3":
+                   ClearTheScreen.clrscr();
+                   ProductBL.show_product_supplier();
+
+                    break;
+                case "0":
+                    ClearTheScreen.clrscr();
+                    Seller();
+                    break;
+
+
+                default:
+                    ClearTheScreen.clrscr();
+                    System.out.println("There is no function for this .Re-Enter:");
+                    break;
+            }
+        }
+
+
+    }
+}
